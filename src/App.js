b6d937aff68830e6e3a3a0ec3b8bef47977f8841
akiju <99388105+akiju@users.noreply.github.com>
2022-04-08 18:14:41 +0900

    20220408 20220408

--- src/App.js
+++ src/App.js
@@ -1,10 +1,28 @@
+import { PrimaryButton } from "./components/atoms/button/PromaryButton";
+import { SecondaryButton } from "./components/atoms/button/SecondaryButton";
+import { SearchInput } from "./components/molecules/SearchInpout";
+import { UserCard } from "./components/organisms/user/Usercard";
 import "./styles.css";
+
+const user = {
+  name: "あっぽー",
+  image: "https://source.unsplash.com/5Vr_RVPfbMI",
+  email: "aaa@bbb.com",
+  phone: "111-2222-3333",
+  company: {
+    name: "株式会社だよ株式会社"
+  },
+  website: "https://google.com"
+};
 
 export default function App() {
   return (
     <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+      <PrimaryButton>ぼたん</PrimaryButton>
+      <SecondaryButton>ぼたん</SecondaryButton>
+      <br />
+      <SearchInput />
+      <UserCard user={user} />
     </div>
   );
 }
